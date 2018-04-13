964dcff08422201f33537b97dd419f719280ecab
pkochanowicz <adres email>
2018-04-13 12:43:40 +0200

    Django i aplikacja

--- canarios/urls.py
+++ canarios/urls.py
@@ -1,4 +1,4 @@
-"""twitter URL Configuration
+"""canarios URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
